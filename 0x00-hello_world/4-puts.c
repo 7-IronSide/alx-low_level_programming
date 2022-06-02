9471c013652523a8ee4c07d7c4b846245770562c
IronsideHellBlazer <meliodas.martin@gmail.com>
2022-06-02 12:31:08 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+char iron[] = "Programing is like building a multilingual puzzle" ;
+
+	puts(iron);
+	
+	return 0 ;
+
+}
