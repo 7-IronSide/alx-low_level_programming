42fdec0e8ab7b3a6305f364d359f2e6b61c97c94
IronsideHellBlazer <meliodas.martin@gmail.com>
2022-06-04 10:42:14 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int i;
+char f;
+f = '\n';
+
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar(f);
+return (0);
+}
