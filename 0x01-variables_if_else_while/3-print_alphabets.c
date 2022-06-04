6c53cf5252909196c3c9a8cd885c281df237278f
IronsideHellBlazer <meliodas.martin@gmail.com>
2022-06-04 05:52:04 -0700

    Print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char i;
+char f;
+f = '\n';
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar (i);
+       	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+	 putchar (i);
+	}
+	putchar (f);
+	return (0);
+}
