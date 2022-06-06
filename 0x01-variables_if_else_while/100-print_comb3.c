41e32cd8ab28d886c521668133375e0d809b1eb7
IronsideHellBlazer <meliodas.martin@gmail.com>
2022-06-06 03:13:28 -0700

    Print comb 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return always 0 (success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 90; i++ )
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+if (i < 89)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
