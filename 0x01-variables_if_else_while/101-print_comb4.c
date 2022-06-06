c8ab1b13e299eed2046e084bf4cf6629e00a1759
IronsideHellBlazer <meliodas.martin@gmail.com>
2022-06-06 11:27:45 -0700

    Print comb 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int i;
+int j;
+int z;
+for (i = '0'; i <= '9'; i++)
+{
+for (j = '0'; j <= '9'; j++)
+{
+for (z = '0'; z <= '9'; z++)
+if (i < j && j < z)
+{
+putchar(i);
+putchar(j);
+putchar(z);
+if ( i < '7' || j < '8' || z < '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
