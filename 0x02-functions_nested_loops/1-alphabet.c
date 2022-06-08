3185eb0ce0c8803d062118bc9b94f71d3f31b5b4
IronsideHellBlazer <meliodas.martin@gmail.com>
2022-06-08 16:50:43 -0700

    alph

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,15 @@
 /**
  * print_alphabet - check the code
  *
- *@c: print one
- *
- * Return: 0.
+ * Return: Nothing
  */
 
 void print_alphabet(void)
 {
-int c;
-for (c = 'a'; c <= 'z'; c++);
+int n;
+for (n = 'a'; n <= 'z'; n++);
+{
 _putchar(c);
+}
 _putchar('\n');
-return (0);
 }
