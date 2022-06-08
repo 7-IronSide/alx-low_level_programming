65c505956fd2d9197edb4530671c1cd504b306f5
IronsideHellBlazer <meliodas.martin@gmail.com>
2022-06-08 15:50:48 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,7 +14,7 @@ while (hour < 24)
 minute = 0;
 while (minute < 60)
 {
-_putcchar((hour / 10) + '0');
+_putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
 _putchar(':');
 _putchar((minute / 10) + '0');
