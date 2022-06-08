0b9c08dc56438178d3b1fb206d4ca9f5d895e00e
IronsideHellBlazer <meliodas.martin@gmail.com>
2022-06-08 13:31:24 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_sign - print the signe of a number
+ * _print_sign - print the signe of a number
  *
  * @c: number to check
  *
  * Return: +1 if is positive, 0 if is 0 and -1 if is negative
  */
 
-int print_sign(int c)
+int _iprint_sign(int c)
 {
 if (c > 0)
 {
