6af0d7e4b61e4088c0498c0c73cfc469749bc955
IronsideHellBlazer <meliodas.martin@gmail.com>
2022-06-09 12:38:10 -0700

    new

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,18 +2,13 @@
 /**
 * main - prints if integer is positive or negative
 *
-* @i: print one
-*
 * Return: 0
 */
 
 int main(void)
 {
-	int i;
-
-
+int i;
 i = 0;
 positive_or_negative(i);
-
 return (0);
 }
