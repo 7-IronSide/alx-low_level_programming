8394d89b1b4a80020758c9e4d79a5ec60cbb086f
ironsidehellblazer <meliodas.martin@gmail.com>
2022-06-13 17:24:00 +0200

    new

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,13 +2,15 @@
 /**
  * swap_int - swaps value of twoo integers
  *
+ * @a: variable
+ * @b: variable
  * Return: 0.
  */
 
 void swap_int(int *a, int *b)
 {
-    int new = 0;
-    new = *b;
-    *b = *a;
-    *a = new;
+int new = 0;
+new = *b;
+*b = *a;
+*a = new;
 }
