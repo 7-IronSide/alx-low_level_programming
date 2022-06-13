d9463c473509e7cbc48bc44090445346e2e89976
ironsidehellblazer <meliodas.martin@gmail.com>
2022-06-13 22:43:47 +0200

    new

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,3 +1,10 @@
+#include "holberton.h"
+/**
+ * puts2 - one character out of two, starting with the first one.
+ * @str: tested string
+ * Return: always 0.
+ */
+void puts2(char *str)
 {
 	int i;
 
