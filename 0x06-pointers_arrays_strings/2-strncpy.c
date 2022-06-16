a82827a8a5eeaabe7b5cfce66eee8c939bb4f43c
ironsidehellblazer <meliodas.martin@gmail.com>
2022-06-16 16:24:43 +0200

    new

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,5 +5,20 @@
  *
  * @dest: the string to be appended upon
  *
- * 
+ * @src: the destination string to be concatenate
+ *
+ * @n:the number of bytes to be appended to dest
+ *
+ * Return: pointer to the resulting string dest
  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int index = 0, dest_len = 0;
+
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (dest);
+}
