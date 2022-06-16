611d90f5b44cc974c486306f7517dd9f7e9588f1
ironsidehellblazer <meliodas.martin@gmail.com>
2022-06-16 07:45:08 +0200

    new

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat -  concatenates two strings
+ *
+ * @src: the source string to append to @dest 
+ *
+ * @dest: the destination string to be concatenate
+ *
+ * Return: pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int index = 0, dest_len = 0;
+while (dest[index++])
+{
+    dest_len++;
+}
+for (index = 0; src[index]; index++)
+{
+    dest[dest_len++] = src[index];
+}
+return (dest);
+}
